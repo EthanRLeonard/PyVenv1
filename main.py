22e71a7a146f74f00f921cf37f5ab4ab9877c2e0
EthanRLeonard <ethanrgleonard@gmail.com>
2023-11-08 20:18:41 -0600

    commit message

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ endpoint = HelloWorld().read_hello, methods=["GET"])
 app.include_router(router)
 
 if __name__ == "__main__":
-   uvicorn.run("helloworld:app", host="127.0.0.1", port=8000, reload=True)
+   uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
